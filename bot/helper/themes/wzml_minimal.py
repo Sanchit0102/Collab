5bcb2f506afc4480ac08c373720cc375dbc66dbb
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-03-13 15:45:24 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,10 +2,10 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Repo'
-    ST_BN1_URL = 'https://www.github.com/weebzone/WZML-X'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/WZML_X'
+    ST_BN1_NAME = 'Stream Bot'
+    ST_BN1_URL = 'https://t.me/SilentHelper_bot'
+    ST_BN2_NAME = 'Auto Bot'
+    ST_BN2_URL = 'https://t.me/silentautobot'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
